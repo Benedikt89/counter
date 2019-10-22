9d71439202d3b3513dada5128dcabe9717bc8243
Benedikt89 <constantine.kosta@gmail.com>
2019-10-22 20:10:41 +0300

    Updates

---
+++ precache-manifest.7ebf17a91d4e3d83dd59b04824f1a854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28dcec507f0a49640639fcf0bf70a3b4",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "31485322b1fb98a2bd9c",
+    "url": "/counter/static/css/main.8c8a6c9a.chunk.css"
+  },
+  {
+    "revision": "63181791b16218155cfb",
+    "url": "/counter/static/js/2.c8076f7c.chunk.js"
+  },
+  {
+    "revision": "31485322b1fb98a2bd9c",
+    "url": "/counter/static/js/main.8f7d67fc.chunk.js"
+  },
+  {
+    "revision": "4d25a895ed3f11370198",
+    "url": "/counter/static/js/runtime~main.3a77d02d.js"
+  }
+]);
